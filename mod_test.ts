ef4e7bf7626d23406a34ac388a488758b411625c
Lloyd Francis <lloydaf@gmail.com>
2021-10-16 16:20:36 +0200

    fix: fixing formatting

--- mod_test.ts
+++ mod_test.ts
@@ -1,5 +1,5 @@
 import { getHelloWorld } from "./mod.ts";
-import {assertEquals} from './test_deps.ts'
+import { assertEquals } from "./test_deps.ts";
 
 Deno.test("get hello world", function (): void {
   assertEquals(getHelloWorld(), "\x1b[1mHello World\x1b[22m");
